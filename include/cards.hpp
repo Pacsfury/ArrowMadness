86fa7129b7478e6e87d313e4fbe25436c1d36bd6
Pacsfury <280690649+Pacsfury@users.noreply.github.com>
2026-08-31 16:36:03 +0200

    Optimization
    Delete Card struct and redundant power in unordered map, which converted to a vector

--- include/cards.hpp
+++ include/cards.hpp
@@ -4,13 +4,6 @@
 #include <unordered_map>
 #include <vector>
 
-struct Card {
-    std::string name;
-    std::string power;
-};
-
 void addCard(std::string name);
 
 bool hasCard(std::string name);
-
-std::string getPower(std::string name);
--- src/cards.cpp
+++ src/cards.cpp
@@ -1,26 +1,23 @@
 #include <vector>
 #include <iostream>
 #include <string>
-#include <unordered_map>
 #include <algorithm>
 
 #include "../include/cards.hpp"
 
-static const inline std::unordered_map<std::string, std::string> CARDS = 
+static const inline std::vector<std::string> CARDS = 
 {
-    {"COMMON.livesplusone", "lives++"},
-    {"SPECIAL.extrapoints", "extrapoints"}
+    "COMMON.livesplusone",
+    "SPECIAL.extrapoints"
 };
 
 
-std::vector<Card> deck;
+std::vector<std::string> deck;
 
 void addCard(std::string name) {
-    Card card;
-    card.name = name;
-    auto it = CARDS.find(name);
-    if (it == CARDS.end()) return;
-    card.power = it->second;
+    std::string card;
+    card = name;
+
     if (deck.size() < 4) {
         deck.push_back(card);
     } else {
@@ -29,25 +26,13 @@ void addCard(std::string name) {
 }
 
 bool hasCard(std::string name) {
-    auto it = std::find_if(deck.begin(), deck.end(), [&name](const Card& card) {
-        return card.name == name;
+    auto it = std::find_if(deck.begin(), deck.end(), [&name](const std::string& card) {
+        return card == name;
     });
 
     if (it != deck.end()) {
         return true;
     } else {
         return false;
-    }
-}
-
-std::string getPower(std::string name) {
-    auto it = std::find_if(deck.begin(), deck.end(), [&name](const Card& card) {
-        return card.name == name;
-    });
-
-    if (it != deck.end()) {
-        return it->power;
-    } else {
-        return it->power;
     }
 }
